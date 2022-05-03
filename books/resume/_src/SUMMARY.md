d44308557da818fa6970f0b0f972a4e19d3aac9a
111coding <jwlee1024@gmail.com>
2022-05-03 15:22:55 +0900

    Removed

--- books/resume/_src/SUMMARY.md
+++
@@ -1,5 +0,0 @@
-# Summary
-
-- [RESUME](README.md)
-
-
--- cupertino_source/cupertino_icons_listview/android/app/src/main/kotlin/com/example/cupertino_icons_listview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cupertino_icons_listview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
